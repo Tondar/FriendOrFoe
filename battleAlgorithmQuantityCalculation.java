6044209207c931bb8e87248137d40208609adc5a
Ben <benjamin-phil.roth@gmx.de>
2014-12-01 20:01:40 +0100

    test112

---
+++ battleAlgorithmQuantityCalculation.java
@@ -0,0 +1,26 @@
+
+public class battleAlgorithmQuantityCalculation {
+
+
+    //this class is used to print simple calculations
+    //currently calculatiing the chances for one unit to kill another unit
+    //depending on enemy army size
+    //assumption: basic fight soldiers vs soldiers, no stats and buffs applied
+    
+    
+    public static void main(String[] args) {
+        int i = 1;
+        int j = 1;
+        double c;
+        double p;
+        
+        while (j < 100) {
+            c = i / 2j;
+            d = c * 100;
+            System.out.println(i + "vs" + j + "Chances for i:" + d);
+            
+        }
+        
+    }
+    
+}
